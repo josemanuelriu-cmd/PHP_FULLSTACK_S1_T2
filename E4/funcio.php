9eda456de9bd3aebca761f2f05bc89a0c10251ce
Riu <josemanuelriu@gmail.com>
2025-11-29 10:45:24 +0100

    Exercise 4

---
+++ E4/funcio.php
@@ -0,0 +1,29 @@
+<?php
+	
+	$num = $_POST["num"];
+	$increment = $_POST["increment"];
+	$resultat;
+		
+	$boleanNum = is_numeric($num);
+	$boleanIncrement = is_numeric($increment);	
+
+	if ($num == '') {
+		$num = 10;
+		$boleanNum = true;
+	} 
+
+	if ($increment == '') {
+		$increment = 1;
+		$boleanIncrement = true;
+	}
+	if (!$boleanNum ) {	
+		echo "Error, el valor no és un número";	
+	} else {
+		echo "El número fins a on comptar es $num <br>";
+		echo "L'increment es de $increment <br><br>";
+		for ($resultat = 1; $resultat <= $num; $resultat+=$increment) {
+			echo "El número es: $resultat<br>";
+		}		
+	}
+
+?>
