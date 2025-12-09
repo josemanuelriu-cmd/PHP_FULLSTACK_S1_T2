5e2c9ef1c8c7e4f1b73e040af24fe6e3ce8080b5
Riu <josemanuelriu@gmail.com>
2025-12-09 12:54:35 +0100

    S1 T2 E3 - Added modul S1 T2 E3b - Added function validation

--- N1/E3/funcio.php
+++ N1/E3/funcio.php
@@ -4,34 +4,47 @@
 	$num2 = $_POST["num2"];
 	$operacio = $_POST["operacio"];
 	
-	$bolean1 = is_numeric($num1);
-	$bolean2 = is_numeric($num2);
-	
-	if ($num1 == '' or $num2 == '' ) {
-		echo "Error, ha d'indicar els 2 valors";
-	} elseif (!$bolean1) {	
-		echo "Error, el valor 1 no és un número";
-	} elseif (!$bolean2) {	
-		echo "Error, el valor 2 no és un número";
-	} else {
+	if (Validar($num1) and Validar($num2)){
 		$resultat = Calcular($num1, $num2, $operacio);		
 		echo "El resultat es igual a $resultat";
 	}
 
 	function Calcular($num1, $num2, $operacio){
-		if ($operacio == '+' ) {
-			$resultat = $num1 + $num2;
-		} elseif ($operacio == '-') {
-			$resultat = $num1 - $num2;
-		} elseif ($operacio == '*') {
-			$resultat = $num1 * $num2;
-		} else { 
-			if ($num2 != 0) {
-				$resultat = $num1 / $num2;
-			} else {
-				$resultat = "infinit";
-			}	
-		} 
+
+		switch ($operacio){
+			case '+': 
+				$resultat = $num1 + $num2;    
+    			break;
+			case '-': 
+				$resultat = $num1 - $num2;    
+    			break;
+			case '*': 
+				$resultat = $num1 * $num2;    
+    			break;
+			case '%': 
+			case '/':
+				if ($num2 == 0) {
+					$resultat ="infinit";
+				} elseif($operacio=='%') {
+					$resultat = $num1 % $num2;   
+				} 
+				else {
+					$resultat = $num1 / $num2;
+				}
+				break;    			
+  		}
 		return $resultat;
+	}
+
+	function Validar($num):bool{
+		$bolean = is_numeric($num);
+		if ($num == '') {
+			echo "Error, ha d'indicar els valors";
+			return FALSE;
+		} elseif (!$bolean) {	
+			echo "Error, el valor no és un número";
+			return FALSE;
+		}
+		return TRUE;
 	}
 ?>
--- N1/E3/index.php
+++ N1/E3/index.php
@@ -6,36 +6,39 @@ $n = 1.1;
 $m = 2.2;
 
 
-echo "x:" . $x;
-echo "<hr>";
-echo "y:" . $y;
-echo "<hr>";
+echo "x:" . $x . "<br>";
+
+echo "y:" . $y . "<br>";
+
 $suma = $x + $y;
 echo "suma: " . $suma;
 $resta = $x - $y;
 echo " -- resta: " . $resta;
 $producte = $x * $y;
 echo " -- producte: " . $producte;
-$modul = $x / $y;
+$divisio = $x / $y;
+echo " -- divisió: " . $divisio;
+$modul = $x % $y;
 echo " -- modul: " . $modul;
 
+echo "<hr>";
+echo "n:" . $n . "<br>";
 
-echo "<hr><hr>";
-echo "n:" . $n;
-echo "<hr>";
-echo "m:" . $m;
-echo "<hr>";
+echo "m:" . $m . "<br>";
+
 $suma = $n + $m;
 echo "suma: " . $suma;
 $resta = $n - $m;
 echo " -- resta: " . $resta;
 $producte = $n * $m;
 echo " -- producte: " . $producte;
-$modul = $n / $m;
+$divisio = $n / $m;
+echo " -- divisió: " . $divisio;
+$modul = $n % $m;
 echo " -- modul: " . $modul;
 
 
-echo "<hr><hr>";
+echo "<hr>";
 $doblex = $x * 2;
 echo "doble x: " . $doblex;
 $dobley = $y * 2;
@@ -44,7 +47,7 @@ $doblen = $n * 2;
 echo " -- doble n: " . $doblen;
 $doblem = $m * 2;
 echo " -- doble m: " . $doblem;
-echo "<hr>";
+echo "<br>";
 $suma = $x + $y + $n + $m;
 echo "suma: " . $suma;
 $producte = $x * $y * $n * $m;
